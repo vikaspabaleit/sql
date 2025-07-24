b739ea57f60dcdffb62bab78f44147de3a4ca0eb
Vikas S Pabale <vikaspabaleit@gmail.com>
2025-07-24 12:57:18 +0530

    Update second_largest_salary.sql

--- second_largest_salary.sql
+++ second_largest_salary.sql
@@ -1,1 +1,8 @@
 #•	To find the second largest salary from a table employee?
+
+WITH salary_data AS (
+  seelct emp_name,emp_id,salary,
+  DENSE_RANK() over(order by salary DESC ) rank_sal from employee 
+  )
+select emp_name,emp_id,salary from salary_data 
+  where rank_sal = 2; 
